ddd3d1bb4aaa971bfc712f17cfc74f42f8530c4e
AlleckMH <alleckmh94@gmail.com>
2023-10-04 11:21:56 -0700

    example code

--- src/components/pages/ExampleCode.js
+++ src/components/pages/ExampleCode.js
@@ -12,13 +12,14 @@ export default function ExampleCode() {
       Here is an example of my code used in the game. The code provided is the 
       motor script for the enemy UFO. It controls movements, attacks, audio data,
       and some UI animations.
-      <object data="/public/images/UFOMotor.pdf" type="application/pdf" width="100%" height="100%">
-        <p>Alternative text - include a link <a href="/public/images/UFOMotor.pdf">to the PDF!</a></p>
-      </object>
       </h5>      <p className='examplecodeP'>
       If you would like the full CS file, click the download below!
       </p>
 
+      <object data="/public/images/UFOMotor.pdf" type="application/pdf" width="100%" height="100%">
+        <p>Alternative text - include a link <a href="/public/images/UFOMotor.pdf">to the PDF!</a></p>
+      </object>
+      
       <Button buttonStyle='btn--outline1' onClick={() => { window.location.href = "https://www.instagram.com/cryptid_games_studio/"; } }>
         Instagram
       </Button>
